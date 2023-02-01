5c4bb0ea52a2334be5ac1b6d4b90fa32981d65de
ShreyasPrakash23 <123919957+ShreyasPrakash23@users.noreply.github.com>
2023-02-01 18:56:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/liveChart.dart';
+void main() => runApp(MyApp());
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class MyApp extends StatelessWidget{
+  const MyApp({Key ? key}) : super(key:key);
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
-        body: Column(
-           children: [
-             titleSection,
-             dataSection,
-             dataSection1,
-           ],
-        ),
-      ),
+      title: 'Flutter Graphs',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: LiveChartWidget(),
     );
   }
 }
-
-Widget titleSection = const Center(
-  child: Text("Slave Device - 1"),
-);
-Widget dataSection = Row(
-  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  children: [
-    _buildButtonColumn(Colors.blue,'Temperature'),
-    _buildButtonColumn(Colors.blue,'Humidity'),
-  ],
-);
-
-Widget dataSection1 = Row(
-  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  children: [
-    _buildButtonColumn(Colors.blue, 'Moisture'),
-  ],
-);
-
-Column _buildButtonColumn(Color color, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
